39abf3c44dd5863a2e05b6e6598c17b5f00f017f
eliassymons <111077574+eliassymons@users.noreply.github.com>
2022-08-23 08:59:59 -0700

    hashrouter update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 
@@ -7,10 +7,10 @@ import RouteTree from "./RouteTree.jsx";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <HashRouter>
         <Header />
         <RouteTree />
-      </Router>
+      </HashRouter>
     </div>
   );
 }
